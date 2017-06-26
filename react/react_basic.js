67d601a626b14258f66e2d24a0b5f41d35c1c677
Charlie Zhu <goodgoodwish@gmail.com>
2017-06-26 00:22:30 -0700

    onClick,

--- react/react_basic.js
+++ react/react_basic.js
@@ -12,7 +12,7 @@
 
 function ListItem (props) {
 	return (
-		<li>
+		<li onClick={props.onClick}>
 			{props.item}
 		</li>
 	);
@@ -25,12 +25,19 @@ class ShoppingList extends React.Component {
 			list: ['Bread', 'Butter', 'Water']
 		};
 	}
+
+	onClick(index) {
+		var newList = this.state.list.slice();
+		newList.splice(index, 1);
+		this.setState({list: newList});
+	}
+	
 	render() {
 		var listItems = [];
 		this.state.list.forEach( (currVal, i) => {
-			listItems.push(<ListItem item={currVal} />);
+			listItems.push(<ListItem item={currVal} onClick={() => this.onClick(i)} />);
 		});
-		
+
 		return (
 			<div className='shopping-list'>
 				<h1>Shopping List for {this.props.name}</h1>
